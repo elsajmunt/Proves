8e9023a39bf58930eead1b8703b3795e6a5d6949
laclipi <162316803+laclipi@users.noreply.github.com>
2024-04-08 12:21:01 +0200

    Add files via upload

---
+++ formularios.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<body>
+<h1> ejercicio 2php </h1>
+
+if (isset ) $_GET.[numero1+numero2]
+
+
+<?php
+$num1= $_POST ["num1"]
+$num1= $_POST ["num1"]
+
+echo "la suma de $num1 y $num2 es: "suma ($num1,$num2)
+
+function suma ($num1, $num2) {
+
+    if ($num1 && $num2;)
+    else
+return "no se puede"
+}
+?>
+
+<form  method = "post">
+
+<label> numero 1 : </label>
+<input type ="number" name = "numero1"><br>
+<label> numero 2 : </label>
+<input type ="number" name = "numero2"><br>
+
+<input type = "submit" name = send value = "calcula">
+
+
+
+</html>
